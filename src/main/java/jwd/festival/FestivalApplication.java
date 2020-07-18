f6a9c1b997abf8f3095ec9286dc87a6d965a0810
Zeljko Stevanovic <doctorpauk@gmail.com>
2020-07-18 21:41:51 +0200

    spring-boot-starter-parent version 1.2.7.RELEASE

--- src/main/java/jwd/festival/FestivalApplication.java
+++ src/main/java/jwd/festival/FestivalApplication.java
@@ -1,18 +1,18 @@
 package jwd.festival;
 
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class FestivalApplication 
 	extends SpringBootServletInitializer {
 	
 //	@SuppressWarnings("unused")
-//	@Autowired 
+	@Autowired 
 //	private TestData td;
-
+	
 	public static void main(String[] args) {
 		 SpringApplication.run(FestivalApplication.class, args);
 	}
